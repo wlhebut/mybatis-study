f4a4d0cd4ffd94ed05967bef475e47020db13410
wltay <249136371@qq.com>
2021-02-18 11:36:53 +0800

    main

---
+++ src/main/java/com/ta/MybatisMain.java
@@ -0,0 +1,13 @@
+package com.ta;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisMain.class,args);
+    }
+}
